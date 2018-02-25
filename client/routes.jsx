2e581279653f40503b146e77d6298c9c9d2870e9
Levy Medina II <levymedina3@gmail.com>
2018-02-26 03:21:30 +0800

    added updated routes

--- client/routes.jsx
+++ client/routes.jsx
@@ -1,1 +1,3 @@
 import { exposedRoutes } from './route-groups/exposed-routes'
+import { securedRoutes } from './route-groups/secured-routes'
+import { adminRoutes } from './route-groups/admin-routes'
