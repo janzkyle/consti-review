a20614b5e2630442f32d286264a386e90266815d
Levy Medina II <levymedina3@gmail.com>
2018-03-18 23:43:39 +0800

    removed account seeder

--- imports/api/accounts.jsx
+++ imports/api/accounts.jsx
@@ -1,2 +1,7 @@
 import { Mongo } from 'meteor/mongo'
 import { Accounts } from 'meteor/accounts-base'
+
+// Accounts.createUser({
+//   email: 'levymedina3@gmail.com',
+//   password: 'Levy8650641!',
+// })
